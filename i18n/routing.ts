4dd80f4db77cd34ce3d2d3bbe4ff88c76adcc73c
Aya Elsayed <127871917+ayanawar4@users.noreply.github.com>
2026-06-07 14:39:11 +0300

    fix: clean build - proxy.ts + force-dynamic

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,6 +1,7 @@
-import {defineRouting} from 'next-intl/routing';
+import { defineRouting } from 'next-intl/routing';
 
 export const routing = defineRouting({
   locales: ['en', 'ar'],
-  defaultLocale: 'en'
+  defaultLocale: 'en',
+  localePrefix: 'always',
 });

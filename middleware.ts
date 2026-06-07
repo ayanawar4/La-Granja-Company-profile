491f5feb6fc037e56ccb2139b9d5d60e0d7dfa5d
Aya Elsayed <127871917+ayanawar4@users.noreply.github.com>
2026-06-07 14:32:12 +0300

    Update Next.js bilingual La Granja site

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,8 @@
 import createMiddleware from 'next-intl/middleware';
+import { routing } from './i18n/routing';
 
-export default createMiddleware({
-  locales: ['en', 'ar'],
-  defaultLocale: 'en',
-  localePrefix: 'always'
-});
+export default createMiddleware(routing);
 
 export const config = {
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
+  matcher: ['/', '/(ar|en)/:path*']
 };
